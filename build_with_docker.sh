fb55dd83ba26a1e5037b65f8a7b6e83d6ed5f70d
reiki4040 <reiki4040@gmail.com>
2016-09-28 20:30:38 +0900

    added build with doccker

---
+++ build_with_docker.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#--------------------------------------------------------------#
+# build rnzoo on golang docker image.
+# 1. create golang and glide enabled docker image.
+# 2. mount local rnzoo directory on container.
+# 3. build rnzoo with build.sh on container.
+# 4. stored binary to mounted directory.
+# 5. got rnzoo binary that compiled docker image go version.
+#--------------------------------------------------------------#
+
+function usage() {
+  cat <<_EOB
+rnzoo build script with docker.
+
+  - build rnzoo binary.
+  - create release archive and show sha256 (for homebrew formula)
+
+[Options]
+  -a: create archive for release
+  -g: run glide up
+
+_EOB
+}
+
+function build() {
+  local opt=""
+  if [ $mode = "archive" ]; then
+    opt="-a"
+  fi
+
+  if [ -n "$glideup" ]; then
+    opt="$opt -g"
+  fi
+
+  # build golang + glide image
+  docker build -t myglide:latest .
+
+  # run rnzoo build with docker
+  docker run --rm \
+		  -v $GOPATH/src/github.com/reiki4040/rnzoo:/go/src/github.com/reiki4040/rnzoo \
+		  -w /go/src/github.com/reiki4040/rnzoo \
+		  myglide:latest bash build.sh $opt
+}
+
+mode="build"
+glideup=
+while getopts agh OPT
+do
+  case $OPT in
+    a) mode="archive"
+       ;;
+    g) glideup="1"
+       ;;
+    h) usage
+       exit 0
+       ;;
+    *) echo "unknown option."
+       usage
+       exit 1
+       ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+build
